cd4e2674dfcd7493b6f1ee1ef971cd6b4c838914
KyleS22 <25314187+KyleS22@users.noreply.github.com>
2019-11-17 22:23:34 -0600

    Add googletest and ctest config

--- test/test_foo.cc
+++ test/test_foo.cc
@@ -1,13 +1,20 @@
 #include <foo.h>
 #include <iostream>
-
+#include "gtest/gtest.h"
 using namespace std;
 
-int main(){
-
-	cout << "Hello World!\n";
-
-	KyleCStructs::Foo test(10);
-
-	test.printSomething();
+namespace{
+	TEST(FooTest, TheTest){
+		EXPECT_EQ(1, 1);
+	//	EXPECT_EQ(1, 0);
+	}
 }
+
+//int main(){
+//
+//	cout << "Hello World!\n";
+//
+//	KyleCStructs::Foo test(10);
+//
+//	test.printSomething();
+//}
